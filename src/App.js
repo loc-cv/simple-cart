470823eeaaa278c149d13762f9159b6506514c4a
loc-cv <donbb6824@gmail.com>
2022-07-27 09:24:03 +0700

    Add CartItem and CartContainer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import CartContainer from "./features/cart/CartContainer";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
