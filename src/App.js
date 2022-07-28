12649a15a633caf8f0fab050ec5295d3ec5d91d6
loc-cv <donbb6824@gmail.com>
2022-07-28 15:50:42 +0700

    Using createAsyncThunk to fetch cartItems from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import CartContainer from "./features/cart/CartContainer";
+import { getCartItems } from "./features/cart/cartSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const { isOpen } = useSelector((state) => state.modal);
+  const { isLoading } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
